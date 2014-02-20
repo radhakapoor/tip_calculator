5b87a5fd36855993c3e86faa7fc29189a65aa6b1
radha kapoor <radha@radhakapoor.net>
2014-02-20 12:49:36 -0500

    user supplied variables with raw_input()

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,13 +1,19 @@
-meal = 50
 tax = 0.08
 tip = 0.15
 
-tax_value = tax * meal
-meal_with_tax = meal + tax_value
+
+
+print "Enter the cost of your meal" 
+meal = raw_input()
+
+tax_value = tax * float(meal)
+print "You need to pay " + "$" + '%.2f' % tax_value + " for tax."
+
+meal_with_tax = float(meal) + tax_value
 tip_value = tip * meal_with_tax
 total  = meal_with_tax + tip_value
-
-print "The base cost of your meal was " + "$" + '%.2f' % meal
-print "You need to pay " + "$" + '%.2f' % tax_value + " for tax."
+print "Without tip but including tax, the cost of your meal is " + "$" + '%.2f' % meal_with_tax
 print "Tipping at a rate of 15%, you should leave " + "$" + '%.2f' % tip_value + " for a tip." 
 print "The grand total for your meal, inclusive of tax and tip is " + "$" + '%.2f' % total + "."
+
+
