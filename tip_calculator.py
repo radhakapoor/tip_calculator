63ab94da604b53c98d47692d7b8a48a00d0af338
radha kapoor <radha@radhakapoor.net>
2014-02-20 12:56:39 -0500

    small revisions to include tax rate and tip preference as raw_inputs as per sample

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,19 +1,22 @@
-tax = 0.08
-tip = 0.15
+
 
 
 
 print "Enter the cost of your meal" 
 meal = raw_input()
+print "Enter the tax rate as a decimal"
+tax = raw_input()
+print "Enter your preferred tip rate"
+tip = raw_input()
 
-tax_value = tax * float(meal)
+tax_value = float(tax) * float(meal)
 print "You need to pay " + "$" + '%.2f' % tax_value + " for tax."
 
 meal_with_tax = float(meal) + tax_value
-tip_value = tip * meal_with_tax
+tip_value = float(tip) * meal_with_tax
 total  = meal_with_tax + tip_value
 print "Without tip but including tax, the cost of your meal is " + "$" + '%.2f' % meal_with_tax
-print "Tipping at a rate of 15%, you should leave " + "$" + '%.2f' % tip_value + " for a tip." 
+print "Tipping at a rate of " + tip +  " you should leave " + "$" + '%.2f' % tip_value + " for a tip." 
 print "The grand total for your meal, inclusive of tax and tip is " + "$" + '%.2f' % total + "."
 
 
