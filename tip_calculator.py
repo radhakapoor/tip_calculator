9a7e2d262606ecb233934eb2bfca53486d669a07
radha kapoor <radha@radhakapoor.net>
2014-02-20 17:25:33 -0500

    user supplied variables with sys.argv

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,22 +1,20 @@
+import sys 
 
 
 
-
-print "Enter the cost of your meal" 
-meal = raw_input()
-print "Enter the tax rate as a decimal"
-tax = raw_input()
-print "Enter your preferred tip rate"
-tip = raw_input()
-
-tax_value = float(tax) * float(meal)
-print "You need to pay " + "$" + '%.2f' % tax_value + " for tax."
-
-meal_with_tax = float(meal) + tax_value
-tip_value = float(tip) * meal_with_tax
+tax_value = float(sys.argv[2]) * float(sys.argv[1])
+meal_with_tax = float(sys.argv[1]) + tax_value
+tip_value = float(sys.argv[3]) * meal_with_tax
 total  = meal_with_tax + tip_value
+
+print "The cost of your meal (without tip or tax) is $", sys.argv[1]
+print "The tax rate is", sys.argv[2]
+print "Your preferred tip rate is", sys.argv[3]
+
+
+print "You need to pay " + "$" +'%.2f' % tax_value + " for tax."
 print "Without tip but including tax, the cost of your meal is " + "$" + '%.2f' % meal_with_tax
-print "Tipping at a rate of " + tip +  " you should leave " + "$" + '%.2f' % tip_value + " for a tip." 
-print "The grand total for your meal, inclusive of tax and tip is " + "$" + '%.2f' % total + "."
+print "Tipping at a rate of " + sys.argv[3] + "," + " you should leave " + "$" + '%.2f' % tip_value + " for a tip." 
+print "The grand total for your meal, inclusive of tax and tip, is " + "$" + '%.2f' % total + "."
 
 
