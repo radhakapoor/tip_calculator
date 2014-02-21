97022479b33c3bbff871815dd6d0e94ee0ac8db8
radha kapoor <radha@radhakapoor.net>
2014-02-21 13:38:49 -0500

    User supplied variables with optparse

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,20 +1,34 @@
-import sys 
+from optparse import OptionParser
 
+parser = OptionParser()
 
+parser.add_option("-f", "--first", dest="first_arg", help="the first argument")
+parser.add_option("-s", "--second", dest="second_arg", help="the second argument")
+parser.add_option("-t", "--third", dest="third_arg", help="the third argument", default="0.15")
 
-tax_value = float(sys.argv[2]) * float(sys.argv[1])
-meal_with_tax = float(sys.argv[1]) + tax_value
-tip_value = float(sys.argv[3]) * meal_with_tax
-total  = meal_with_tax + tip_value
+(options, args) = parser.parse_args()
 
-print "The cost of your meal (without tip or tax) is $", sys.argv[1]
-print "The tax rate is", sys.argv[2]
-print "Your preferred tip rate is", sys.argv[3]
+if not (options.first_arg):
+    parser.error("You need to provide the meal value")
+if not (options.second_arg):
+    parser.error("Your need to provide a tax rate")
+    
+meal = format(options.first_arg)
+tax = format(options.second_arg)
+tip = format(options.third_arg)
+tax_value = float(tax) * float(meal)
+meal_with_tax = float(meal) + tax_value
+tip_value = float(meal) * float(tip)
+total = meal_with_tax + tip_value
 
+print "The price of the meal is $" + meal
+print "The tax rate is " + tax + "."
+print "The tip rate is " + tip + "."
 
 print "You need to pay " + "$" +'%.2f' % tax_value + " for tax."
 print "Without tip but including tax, the cost of your meal is " + "$" + '%.2f' % meal_with_tax
-print "Tipping at a rate of " + sys.argv[3] + "," + " you should leave " + "$" + '%.2f' % tip_value + " for a tip." 
+print "Tipping at a rate of " + tip + "," + " you should leave " + "$" + '%.2f' % tip_value + " for a tip." 
 print "The grand total for your meal, inclusive of tax and tip, is " + "$" + '%.2f' % total + "."
 
 
+#type in shell python optionparser.py -f 50
